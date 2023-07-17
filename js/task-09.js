27f4268cff0cef58f650b947c976976f0a9fac96
Андрій <122921284+AndrijAyzy@users.noreply.github.com>
2023-07-17 22:41:58 +0300

    fg

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,13 @@
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
 changeColorButton.addEventListener('click', () => {
   const color = getRandomHexColor();
   body.style.backgroundColor = color;
